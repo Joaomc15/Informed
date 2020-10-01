93c9dfa5e8366eed5f78e9f33cbf9c495b8450f3
Joao Mocelin Carvalho <56238440+Joaomc15@users.noreply.github.com>
2020-10-01 09:34:44 -0400

    Update webscrap.js

--- scripts/webscraper/webscrap.js
+++ scripts/webscraper/webscrap.js
@@ -44,26 +44,43 @@ function createElectionPinecrest() {
 function getCandidates() {
     let table = document.querySelector("#widget_4_2815_2710 > div > table");
     let candidates = [];
+    let temp = [];
 
-    for (var i = 0, row; row = table.rows[i]; i++) {
-        //iterate through rows
-        //rows would be accessed using the "row" variable assigned in the for loop
-        let temp = [];
-        console.log("Hello");
+    // for (var i = 0, row; row = table.rows[i]; i++) {
+    //     //iterate through rows
+    //     //rows would be accessed using the "row" variable assigned in the for loop
+    //     let temp = [];
+    //     console.log("Hello");
 
         //TODO this if isn't working.
-        // if (!document.activeElement.querySelector("td strong")) { 
+        // if (document.activeElement.querySelector("p a")) { 
+        for (let i = 2, yes; i < 9; i++){
+            let tr = document.querySelector(`#widget_4_2815_2710 > div > table > tbody > tr:nth-child(${i}) > td:nth-child(1) > p > a`);
+            console.log("hellow orld");
+            if(tr){
+                // console.log(tr.textContent);
+                temp.push(tr.textContent);
+                
+            }else {
+                console.log(temp);
+                candidates.push(temp);
+                temp = [];
+                console.log(temp);
 
-        console.log("I'm Working");
-        for (var j = 0, col; col = row.cells[j]; j++) {
-            //iterate through columns
-            //columns would be accessed using the "col" variable assigned in the for loop
-            temp.push(document.activeElement.querySelector("p a").textContent);
-            console.log(temp);
+            }
+            
         }
-        candidates.push(temp)
-        // }
-
+       
+        // for (var j = 0, col; col = row.cells[j]; j++) {
+        
+        //iterate through columns
+        //columns would be accessed using the "col" variable assigned in the for loop
+        // temp.push(document.activeElement.querySelector("p a").textContent);
+        // console.log("I'm Working");
+        // console.log(temp);
+        // // }
+        // candidates.push(temp)
+        // // } 
+        return candidates;
     }
-    return candidates;
-}
+   
